21adcd671f03aba64bc003bc8ab4fb68a4b5fa52
kittiphop sompuech <admin@kittiphops-MacBook-Air.local>
2026-01-23 10:06:16 +0700

    feat: update project for build

---
+++ app/approved/[id]/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import MainLayout from '@/src/components/layout/MainLayout';
+import UnderDevelopment from '@/src/components/common/UnderDevelopment';
+import { useTranslation } from 'react-i18next';
+
+export default function ApprovedDetailPage() {
+  const { t } = useTranslation('common');
+
+  return (
+    <MainLayout title={t('menu.assetApproved')}>
+      <UnderDevelopment pageName={t('menu.assetApproved')} />
+    </MainLayout>
+  );
+}
